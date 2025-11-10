f40c7cb657273c042e973eaea9b532d603ba4abd
Wu Long 🍥 <callmewulong@gmail.com>
2025-11-11 04:54:03 +0700

    Update partner_key_config.php

--- config/partner_key_config.php
+++ config/partner_key_config.php
@@ -1,6 +1,7 @@
 <?php
 // ✨ PHP By Wu Long -->
 // Bảo Mật Key
-define("PARTNER_ID", "59479332748");     // 🆔 ID API của bạn
-define("SECRET_KEY", "6814c0a4c360525b1426a8c5a06fff19"); // 🔑 Secret Key của bạn
+define("PARTNER_ID", "59478262748");     // 🆔 ID API của bạn
+define("SECRET_KEY", "6814c0a4c265525b1426a8c5a06fff19"); // 🔑 Secret Key của bạn
 ?>
+
